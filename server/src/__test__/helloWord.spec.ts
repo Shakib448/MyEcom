609a7817424bdbe2df4c608ef1186b797234b951
Shakiba448 <shakiba448@gmail.com>
2021-11-29 11:21:43 +0600

    WIP on testing

--- server/src/__test__/helloWord.spec.ts
+++ server/src/__test__/helloWord.spec.ts
@@ -1,8 +1,5 @@
-import graphQLRequest from "../Utils/graphRequest";
-
 describe("Mutation", () => {
-  it("should test graphQl structure by printing hello world!", async () => {
-    const helloWorld = await graphQLRequest(`query { helloWorld }`);
-    expect(helloWorld.body.data.helloWorld).toBe("👋 Hello world! 👋");
+  it("Husky should work well", () => {
+    console.log("test");
   });
 });
