901ec3afab8f2462840f83bcd4364fd99cbbc18b
Shakiba448 <shakiba448@gmail.com>
2021-11-29 21:58:04 +0600

    WIP on User input validation

--- server/src/resolvers/User.resolver.ts
+++ server/src/resolvers/User.resolver.ts
@@ -1,11 +1,12 @@
 import { IResolvers } from "@graphql-tools/utils";
+import joi from "joi";
 
 const resolverMap: IResolvers = {
   Query: {
     getAllUsers: () => {},
   },
   Mutation: {
-    userCreate: (_: void): any => {
+    userCreate: (_: void, args: void): any => {
       return {
         success: true,
         message: "User created successfully",
