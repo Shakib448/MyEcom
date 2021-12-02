a9a32259485a77cdd54734693665a034ce8a9919
Shakiba448 <shakiba448@gmail.com>
2021-12-02 12:26:04 +0600

    WIP on user

--- server/src/__test__/User.spec.ts
+++ server/src/__test__/User.spec.ts
@@ -11,7 +11,7 @@ describe("User testing", () => {
     await stopDB();
   });
   it("User will be created with success message", async () => {
-    const res = await graphQLRequest(`mutation UserCreate {
+    const req = await graphQLRequest(`mutation UserCreate {
       userCreate(
         email: "test@test.com"
         password: "12345678"
@@ -44,8 +44,47 @@ describe("User testing", () => {
       }
     }`);
 
-    expect(res.body.data.userCreate.message).toBe("User created successfully");
-    expect(res.body.data.userCreate.success).toBe(true);
-    expect(res.body.data.userCreate.user).toBeInstanceOf(Object);
+    expect(req.body.data.userCreate.message).toBe("User created successfully");
+    expect(req.body.data.userCreate.success).toBe(true);
+    expect(req.body.data.userCreate.user).toBeInstanceOf(Object);
+  });
+
+  it("User will be created with success message", async () => {
+    const req = await graphQLRequest(`mutation UpdateUser {
+      updateUser(
+        email: "test@test.com"
+        password: "test12345678"
+        confirmPassword: "test12345678"
+        firstName: "testFirst name"
+        lastName: "Test"
+        phoneNumber: "1234567"
+        address: "test"
+        city: "test"
+        country: "test"
+        state: "test"
+        location: "test"
+        zip: "12345"
+      ) {
+        message
+        success
+        user {
+          id
+          email
+          firstName
+          lastName
+          phoneNumber
+          address
+          city
+          zip
+          location
+          state
+          country
+        }
+      }
+    }`);
+
+    expect(req.body.data.updateUser.message).toBe("User updated successfully");
+    expect(req.body.data.updateUser.success).toBe(true);
+    expect(req.body.data.updateUser.user).toBeInstanceOf(Object);
   });
 });
--- server/src/resolvers/User.resolver.ts
+++ server/src/resolvers/User.resolver.ts
@@ -1,5 +1,6 @@
 import { IResolvers } from "@graphql-tools/utils";
 import { UserInputError } from "apollo-server-core";
+
 import { userValidationSchema } from "../interface/User.interface";
 import User from "../models/User.model";
 import generateToken from "../Utils/generateToken";
@@ -39,6 +40,51 @@ const resolverMap: IResolvers = {
           success: true,
           message: "User created successfully",
           user: { ...args, id: user._id, token: generateToken(user._id) },
+        };
+      }
+    },
+    updateUser: async (_: void, args: any) => {
+      const { error } = userValidationSchema.validate(args, {
+        abortEarly: false,
+      });
+
+      if (error) {
+        throw new UserInputError(
+          "Failed to create a character due to validation error",
+          { validationError: error.details }
+        );
+      }
+
+      const user: any = await User.findOne({ email: args.email });
+
+      if (!user) {
+        throw new UserInputError("User not found");
+      }
+
+      if (user) {
+        user.email = args.email;
+        user.firstName = args.firstName;
+        user.lastName = args.lastName;
+        user.phoneNumber = args.phoneNumber;
+        user.address = args.address;
+        user.city = args.city;
+        user.country = args.country;
+        user.state = args.state;
+        user.location = args.location;
+        if (args?.password) {
+          user.password = args.password;
+        }
+
+        const updatedUser = await user.save();
+
+        return {
+          success: true,
+          message: "User updated successfully",
+          user: {
+            ...args,
+            id: updatedUser._id,
+            token: generateToken(updatedUser._id),
+          },
         };
       }
     },
