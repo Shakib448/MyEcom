88d2442e1ee2250c988edff3b34d137daeae518e
Shakiba448 <shakiba448@gmail.com>
2021-12-05 17:44:42 +0600

    WIP on user testing

--- server/src/Utils/graphRequest.ts
+++ server/src/Utils/graphRequest.ts
@@ -2,7 +2,6 @@ import app from "./server";
 import supertest from "supertest";
 
 const graphQLRequest = (query: string, token?: string) => {
-  console.log("token", token);
   return supertest(app)
     .post("/graphql")
     .send({
