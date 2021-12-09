b712ac1fd5d2c757056cf0894072f5025765a084
Shakiba448 <shakiba448@gmail.com>
2021-12-09 20:36:06 +0600

    WIP on products

--- server/src/__test__/Product.spec.ts
+++ server/src/__test__/Product.spec.ts
@@ -26,6 +26,18 @@ describe("Product Testing", () => {
         }
       }`);
 
+    res.body.data.getAllProducts.map((item: ProductInterface) => {
+      expect(typeof item.id).toBe("string");
+      expect(typeof item.name).toBe("string");
+      expect(typeof item.image).toBe("string");
+      expect(typeof item.description).toBe("string");
+      expect(typeof item.brand).toBe("string");
+      expect(typeof item.category).toBe("string");
+      expect(typeof item.price).toBe("number");
+      expect(typeof item.countInStock).toBe("number");
+      expect(typeof item.rating).toBe("number");
+      expect(typeof item.numReviews).toBe("number");
+    });
     expect(Array.isArray(res.body.data.getAllProducts)).toBe(true);
     expect(res.statusCode).toBe(200);
   });
