0f948af5731f7275739cac48d5ba42678fc69a01
Shakiba448 <shakiba448@gmail.com>
2021-12-09 18:03:45 +0600

    WIP on products

--- server/src/Utils/seeder.ts
+++ server/src/Utils/seeder.ts
@@ -1,16 +1,30 @@
-import users from "../data/users.data";
 import "colors";
+
+import users from "../data/users.data";
+import products from "../data/products.data";
 import User from "../models/User.model";
+import Product from "../models/Product.model";
 import { connectDB } from "../config/db";
 
 connectDB();
 
 const importData = async () => {
-  await User.deleteMany();
-
-  await User.insertMany(users);
-
   try {
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+
+    const adminUser = createdUsers[0]._id;
+
+    const sampleProducts = products.map((product) => {
+      return {
+        ...product,
+        user: adminUser,
+      };
+    });
+
+    await Product.insertMany(sampleProducts);
     console.log("Data imported".green.inverse);
     process.exit();
   } catch (error) {
@@ -21,6 +35,7 @@ const importData = async () => {
 const destroyedData = async () => {
   try {
     await User.deleteMany();
+    await Product.deleteMany();
 
     console.log("Data Destroyed".red.inverse);
     process.exit();
