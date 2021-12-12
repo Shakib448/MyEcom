1d990321f9a0aa9b70ff9db1f7ee85fec22cded2
Shakiba448 <shakiba448@gmail.com>
2021-12-12 21:45:40 +0600

    WIP on Products

--- server/src/interface/Product.interface.ts
+++ server/src/interface/Product.interface.ts
@@ -26,6 +26,5 @@ export const productValidationSchema = Joi.object({
   category: Joi.string().label("Category").required(),
   price: Joi.number().label("Price").required(),
   countInStock: Joi.number().label("Count In Stock").required(),
-  rating: Joi.number().label("Raring").required(),
   numReviews: Joi.number().label("Number Reviews").required(),
 });
--- server/src/resolvers/Product.resolver.ts
+++ server/src/resolvers/Product.resolver.ts
@@ -1,5 +1,8 @@
+import { UserInputError } from "apollo-server-core";
 import { IResolvers } from "@graphql-tools/utils";
+import { productValidationSchema } from "../interface/Product.interface";
 import Product from "../models/Product.model";
+import getAuthorizedUser from "../middleware/authMiddleware";
 
 const resolverMap: IResolvers = {
   Query: {
@@ -8,11 +11,29 @@ const resolverMap: IResolvers = {
     },
   },
   Mutation: {
-    createProduct: async (_: void, { product }: any) => {
+    createProduct: async (_: void, { product }: any, { req }: any) => {
+      const { error } = productValidationSchema.validate(product, {
+        abortEarly: false,
+      });
+
+      if (error) {
+        throw new UserInputError(
+          "Failed to create a character due to validation error",
+          { validationError: error.details }
+        );
+      }
+
+      const { user }: any = await getAuthorizedUser(req);
+
+      const newProduct = await Product.create({
+        ...product,
+        user: user._id,
+      });
+
       return {
         success: true,
         message: "Product created successfully",
-        product,
+        product: newProduct,
       };
     },
   },
